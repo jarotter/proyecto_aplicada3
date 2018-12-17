d9173044510251716b8a9d325be2e369b8a2867f
Jorge Rotter <jorgearotter@gmail.com>
2018-12-16 22:20:01 -0600

    Uses ggrepel

--- reports/mmtsne.r
+++ reports/mmtsne.r
@@ -1,7 +1,33 @@
 library(R.matlab)
 library(mmtsne)
+library(tidyverse)
+library(ggrepel)
+
+set.seed(3141)
 
 wa <- readMat('./data/wa1000.mat')
 symwa <- p2sp(wa$P)
-
+labels <- unlist(wa$words)
 maps <- mmtsneP(symwa, no_maps=10)
+
+idx <- sample.int(n=1000, size=100, replace=FALSE)
+
+plots = list()
+
+for(map in c(1:10)){
+  coords <- maps$Y[,,map]
+  x <-  coords[idx,1]
+  y <- coords[idx,2]
+  pts <- data_frame(x,y)
+  
+  plots[[map]] = ggplot(pts, aes(x,y, label=labels[idx])) +
+    geom_text_repel() +
+    geom_point(color='red') +
+    theme_classic()
+}
+
+pdf("plots.pdf")
+for (i in 1:10) {
+  print(plots[[i]])
+}
+dev.off()
